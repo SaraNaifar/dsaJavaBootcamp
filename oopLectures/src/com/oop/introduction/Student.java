862de9ab99c4ee21bec43a3c6aff57338120c8ce
sara naifar <naifarsara@gmail.com>
2022-10-07 00:24:01 +0200

    oop lecture : introduction & concepts

--- oopLectures/src/com/oop/introduction/Student.java
+++ oopLectures/src/com/oop/introduction/Student.java
@@ -1,7 +1,14 @@
 package com.oop.introduction;
 
 public class Student {
-    int rollNumber = 12;
-    String name = "sara";
-    float gpa;
+    int rollNumber = 1;
+    String name = "test";
+    float gpa = 5.5f;
+
+
+    Student(int rno, String naam, float score){
+        this.rollNumber = rno;
+        this.name = naam;
+        this.gpa = score;
+    }
 }
--- oopLectures/src/com/oop/introduction/introducationLecture.java
+++ oopLectures/src/com/oop/introduction/introducationLecture.java
@@ -4,10 +4,18 @@ package com.oop.introduction;
 public class introducationLecture {
 
     public static void main(String[] args) {
-        Student student1 = new Student();
-        System.out.println(student1.rollNumber);
-        System.out.println(student1.name);
-        System.out.println(student1.gpa);
+
+        
+        Student student2 = new Student(11,"sara", 92.3f);
+        System.out.println(student2.rollNumber);
+        System.out.println(student2.name);
+        System.out.println(student2.gpa);
+
+             
+        Student student3 = new Student(26,"nour", 94.53f);
+        System.out.println(student3.rollNumber);
+        System.out.println(student3.name);
+        System.out.println(student3.gpa);
     }
     
 }
